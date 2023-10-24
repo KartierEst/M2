85da7ee3202da32ac224aa20d45a0984f5946d7a
Abder <abderbekhti77@gmail.com>
2023-10-24 14:15:26 +0200

    TP3 android appariements

--- Android/TP3/app/src/main/java/com/example/tp3/Flags.kt
+++ Android/TP3/app/src/main/java/com/example/tp3/Flags.kt
@@ -2,13 +2,13 @@ package com.example.tp3
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -17,6 +17,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tp3.ui.theme.TP3Theme
+import kotlinx.coroutines.delay
 
 class Flags : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,10 +36,13 @@ fun VerticalFillBar(backgroundColor: Color = Color.White, foregroundColor: Color
 @Composable
 fun CountdownBar(initialCountdown: Int, elapsedTime: Int, modifier: Modifier = Modifier){
     val duration = (elapsedTime/initialCountdown)
-    VerticalFillBar(fillRatio = 0.2f)
+    val minutes = initialCountdown / 60 // (-1)
+    val secondes = initialCountdown - (60 * minutes)
+    val text = if(minutes != 0) { "$minutes min $secondes s" } else { "$secondes s" }
+    VerticalFillBar(fillRatio = duration.toFloat())
     Box(modifier.fillMaxWidth(),contentAlignment = Alignment.Center) {
         Text(
-            "20s",
+            text,
             textAlign = TextAlign.Center,
             fontSize = 7.sp,
             color = Color.Black
@@ -48,16 +52,134 @@ fun CountdownBar(initialCountdown: Int, elapsedTime: Int, modifier: Modifier = M
 
 @Composable
 fun Countdown(duration: Int, running: Boolean, onEnd: () -> Unit) {
-
-
+    var counter by rememberSaveable() { mutableStateOf(0) }
+    CountdownBar(counter, duration)
+    if(running) {
+        LaunchedEffect(counter) {
+            while (counter != duration) {
+                delay(1000)
+                counter++
+            }
+            onEnd()
+        }
+    }
 }
+
+@Composable
+fun DurationSelector(currentDuration: Int, presetDurations: List<Int>, onSelectedDuration: (Int) -> Unit) {
+    var openMenu by remember { mutableStateOf(false) }
+    Box(modifier = Modifier) {
+        Text("$currentDuration s")
+        DropdownMenu(expanded = openMenu, onDismissRequest = { openMenu = false }, modifier = Modifier) {
+            presetDurations.forEach { s ->
+                DropdownMenuItem(text = { Text("$s s") },onClick = {
+                    onSelectedDuration(s)
+                    openMenu = false
+                })
+            }
+        }
+    }
+}
+
+@Composable
+fun Chunck(maxWidth: Int, maxHeight: Int, r: MutableState<Int>, c: MutableState<Int>){
+    while(c.value*r.value < 26){
+        r.value += 1
+        c.value = (maxWidth*r.value)/maxHeight
+    }
+}
+
+
+@Composable
+fun AlphaGrid(modifier: Modifier, letterClicked: MutableState<Char?>){
+    var borderColor by remember { mutableStateOf(Color.White) }
+    BoxWithConstraints(modifier=Modifier.fillMaxSize()) {
+        val constraints = this.constraints
+        val r = remember { mutableStateOf(1) }
+        val c = remember { mutableStateOf(((maxWidth*r.value)/maxHeight).toInt())}
+        Chunck(constraints.maxWidth, constraints.maxHeight, r, c)
+        val tab = ('A'..'Z').chunked(c.value)
+        Column {
+            for (lotLetter in tab) {
+                Row {
+                    for (letter in lotLetter){
+                        LetterDisplayer(
+                            letter,
+                            modifier.size(
+                                width = (constraints.maxWidth / c.value * 3).pxToDp().dp,
+                                height = (constraints.maxHeight / r.value * 3).pxToDp().dp,
+                            ).clickable(onClick = {
+                                if(letterClicked.value == null || letterClicked.value != letter) {
+                                    letterClicked.value = letter
+                                    borderColor = Color.Red
+                                }
+                                else {
+                                    borderColor = Color.Black
+                                    letterClicked.value = null
+                                }
+                            }),
+                            Color.Black,
+                            borderColor
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun FlagGrid(modifier: Modifier, flagClicked: MutableState<ICSFlag?>){
+    /*val letterClicked = remember { mutableStateOf<Char?>(null) }*/
+    var borderColor by remember { mutableStateOf(Color.White) }
+    BoxWithConstraints(modifier=Modifier.fillMaxSize()) {
+        val constraints = this.constraints
+        val r = remember { mutableStateOf(1) }
+        val c = remember { mutableStateOf(((maxWidth*r.value)/maxHeight).toInt())}
+        Chunck(constraints.maxWidth, constraints.maxHeight, r, c)
+        val tab = ('A'..'Z').chunked(c.value)
+        Column(modifier) {
+            for (lotLetter in tab) {
+                Row(modifier) {
+                    for (letter in lotLetter){
+                        val flag = ICSFlag.findFlag(letter)
+                        flag?.Flag(modifier.size(
+                            width = (constraints.maxWidth / 12).pxToDp().dp,
+                            height = (constraints.maxHeight / 24).pxToDp().dp
+                        ).clickable(onClick = {
+                            if(flagClicked.value != flag) {
+                                flagClicked.value = flag
+                                borderColor = Color.Red
+                            } else {
+                                borderColor = Color.Black
+                                flagClicked.value = null
+                            }
+                        }).border(width = 1.dp, color = borderColor))
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+@Preview
+@Composable
+fun ParameterizedCountDown(){
+    var duration = 10
+    var running = false
+    Countdown(10,running = running, onEnd = {running = false})
+    DurationSelector(duration, listOf(20, 30, 60, 90), onSelectedDuration = { s -> duration = s })
+    Button(onClick = { running = !running }){Text("RUN")}
+}
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview2() {
+fun Preview() {
     TP3Theme {
+        var running = true
         //Greeting2("Android"
         //VerticalFillBar(fillRatio = 0.2f)
-        CountdownBar(50,10)
+        //CountdownBar(50,10)
     }
 }
